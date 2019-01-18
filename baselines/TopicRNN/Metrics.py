4830071ccc4ea0a412eb9406a4c78e81df6ecc0e
truthless11 <truthless11@gmail.com>
2019-01-18 10:59:57 +0800

    rewrite bleu smoothie

--- baselines/TopicRNN/Metrics.py
+++ baselines/TopicRNN/Metrics.py
@@ -5,12 +5,27 @@ Created on Tue Jan  1 11:56:26 2019
 @author: truthless
 """
 
-from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk.translate.bleu_score import sentence_bleu
 import numpy as np
 import torch.nn.functional as F
 from DataManager import EOS
 
-smoothie = SmoothingFunction().method4
+epsilon = 1e-10 # for numerical stability
+
+def method4(p_n, references, hypothesis, hyp_len, *args, **kwargs):
+    """
+    Smoothing method 4:
+    Shorter translations may have inflated precision values due to having
+    smaller denominators; therefore, we give them proportionally
+    smaller smoothed counts. Instead of scaling to 1/(2^k), Chen and Cherry
+    suggests dividing by 1/ln(len(T)), where T is the length of the translation.
+    """
+    invcnt = 1
+    for i, p_i in enumerate(p_n):
+        if p_i.numerator == 0 and hyp_len != 0:
+            incvnt = invcnt * 5 / np.log(hyp_len + epsilon)
+            p_n[i] = 1 / incvnt
+    return p_n
 
 def bleu(refs, hyps, lens):
     '''
@@ -26,7 +41,7 @@ def bleu(refs, hyps, lens):
         if EOS in hyp:
             index = hyp.index(EOS)
             hyp = hyp[:index]
-        score += sentence_bleu([ref], hyp, smoothing_function=smoothie)
+        score += sentence_bleu([ref], hyp, smoothing_function=method4)
     return score
 
 def language_model_p(refs, word_p, lens):
@@ -34,7 +49,7 @@ def language_model_p(refs, word_p, lens):
     probs = F.softmax(word_p, dim=-1)
     for i, l in enumerate(lens):
         for j in range(l):
-            p += np.log(probs[i, j, refs[i, j]].item() + 1e-100)
+            p += np.log(probs[i, j, refs[i, j]].item() + epsilon)
     return p
 
 def perplexity(p, N):
